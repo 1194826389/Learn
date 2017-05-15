32a811be2f20753bd32e27106a5701ef39558958
hechao <CNiceToUpp@gmail.com>
2017-05-15 11:29:58 +0800

    learn fixdeadlock

--- concurrency/src/main/java/com/learn/basicthread/deadlock/DeadlockingDiningPhilosophersMain.java
+++ concurrency/src/main/java/com/learn/basicthread/deadlock/DeadlockingDiningPhilosophersMain.java
@@ -17,9 +17,20 @@ public class DeadlockingDiningPhilosophersMain {
         for (int i = 0; i < size; i++) {
             sticks[i] = new Chopstick();
         }
+
+        // 有死锁的情况
         for (int i = 0; i < size; i++) {
             exec.execute(new Philosopher(sticks[i],sticks[(i + 1) % size],i,ponder));
         }
+
+//        // 消除循环等待条件，于是没有死锁问题
+//        for (int i = 0; i < size; i++) {
+//            if (i == (size - 1)) {
+//                exec.execute(new Philosopher(sticks[0],sticks[i],i,ponder));
+//            } else {
+//                exec.execute(new Philosopher(sticks[i], sticks[(i + 1) % size], i, ponder));
+//            }
+//        }
         TimeUnit.SECONDS.sleep(10);
         exec.shutdownNow();
     }
